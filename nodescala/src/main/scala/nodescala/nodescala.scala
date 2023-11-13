21b36e78e00ff1cfcc14f2d56406f8aa2f066ab4
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-11-13 16:36:03 +0900

    implement other part

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,13 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (response.hasNext && token.nonCancelled)
+    {
+      exchange.write(response.next)
+    }
+    exchange.close
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +47,20 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+
+    Future.run() {
+      token => async {
+        while (token.nonCancelled) {
+          val (req, xchg) = await(listener.nextRequest())
+          respond(xchg, token, handler(req))
+        }
+        listenerSubscription.unsubscribe()
+      }
+    }
+  }
 
 }
 
@@ -76,7 +95,7 @@ object NodeScala {
 
   object Exchange {
     def apply(exchange: HttpExchange) = new Exchange {
-      val os = exchange.getResponseBody()
+      val os = exchange.getResponseBody() //이게 알아서 해주겠지
       exchange.sendResponseHeaders(200, 0L)
 
       def write(s: String) = os.write(s.getBytes)
@@ -149,7 +168,7 @@ object NodeScala {
 
   /** The standard server implementation.
    */
-  class Default(val port: Int) extends NodeScala {
+  class Default(val port: Int) extends NodeScala {//object NodeScala 안에서 extends NodeScala
     def createListener(relativePath: String) = new Listener.Default(port, relativePath)
   }
 
